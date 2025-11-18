80d94f9629c9bd4ac4f56f4dc36ac2b9da50a277
Divyanshu Singh <singhdivyanshu3456@gmail.com>
2025-11-18 22:25:18 +0530

    Add files via upload

---
+++ TASK1.py
@@ -0,0 +1,8 @@
+Num1 = int(input("Enter a first number: "))
+Num2 = int(input("Enter a second number: "))
+
+
+print ("Addition", Num1 + Num2, sep=": ")
+print("Subtraction", Num1 - Num2, sep=": ")
+print("Multiplication", Num1 * Num2, sep=": ")
+print("Division", Num1 / Num2, sep=": ")
